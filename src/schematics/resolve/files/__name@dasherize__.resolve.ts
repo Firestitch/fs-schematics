055f2464d40765e88d96c870b67e0599310c52a8
Eugene Kubesh <codbasters@gmail.com>
2020-10-21 15:24:06 +0300

    SP-T1908 fix resolve style issues

--- src/schematics/resolve/files/__name@dasherize__.resolve.ts
+++ src/schematics/resolve/files/__name@dasherize__.resolve.ts
@@ -1,9 +1,10 @@
 import { Injectable } from '@angular/core';
 import { Resolve, ActivatedRouteSnapshot } from '@angular/router';
 
-import { toNumber } from 'lodash-es';
-
 import { RouteSubject } from '@firestitch/core';
+
+import { Observable } from 'rxjs';
+import { toNumber } from 'lodash-es';
 
 import { <%= classify(serviceName) %> } from '<%= relativeServicePath %>';
 
@@ -13,16 +14,17 @@ export class <%= classify(name) %>Resolve implements Resolve<any> {
 
   constructor(private _<%= camelize(serviceName)%>: <%= classify(serviceName) %>) {}
 
-  public resolve(route: ActivatedRouteSnapshot) {
+  public resolve(route: ActivatedRouteSnapshot): Observable<RouteSubject> {
     const routeSubject = new RouteSubject();
 
-    if (!toNumber(route.params.<%= name %>_id)) {
+    if (!toNumber(route.params.id)) {
       return routeSubject.next({});
     }
 
     const query = {};
+
     return routeSubject
-      .observe(this._<%= camelize(serviceName) %>.get(route.params.<%= name %>_id, query));
+      .observe(this._<%= camelize(serviceName) %>.get(route.params.id, query));
   }
 
 }
